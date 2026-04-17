72122afe34ec5b45f855c5492ea317152574a578
Andrew Schaffer <andrew@example.com>
2026-04-17 11:58:26 -0400

    Migrate to GitHub Pages and add deployment workflow

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://mnwcuinnshsncqrhvsks.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ud2N1aW5uc2hzbmNxcmh2c2tzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1NzgzMDksImV4cCI6MjA5MTE1NDMwOX0.ClXCeiKk1HeZB8PLdV1FNPcteEvgq5vpXjgZ0fXl284',
+  process.env.EXPO_PUBLIC_SUPABASE_URL,
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
   {
     auth: {
       storage: AsyncStorage,
